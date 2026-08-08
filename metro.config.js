8b411c508cc7df24d98a9143fb8e5d728478f4dd
Vijay Mahes <Vijaypradhap2004@gmail.com>
2026-08-08 07:02:22 +0530

    Fix expo-router root mapping to ./src/app in app.json and metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
+process.env.EXPO_ROUTER_APP_ROOT = process.env.EXPO_ROUTER_APP_ROOT || "./src/app";
+
 const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, { input: "./src/global.css" });
